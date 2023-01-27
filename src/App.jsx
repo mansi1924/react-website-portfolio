727bdd73f863f47184393a011f40f56e3342f640
Mansi2419 <mg5111417@gmail.com>
2023-01-27 13:31:02 +0530

    mansi 4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
- import React from 'react'
- import Header from './components/header/Header'
- import Nav from './components/nav/Nav'
- import About from './components/about/About'
-//  import Experience from './components/experience/Experience'
-//  import Services from './components/services/Services'
- import Portfolio from './components/portfolio/Portfolio'
-//  import Testimonials from './components/testimonials/Testimonials'
- import Contact from './components/contact/Contact'
- import Footer from './components/footer/Footer'
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
+
+const App = () => {
  
- const App = () => {
-   return (
+  return (
     <>
-        <Header />
-        <Nav />
-        <About />
-        <Portfolio />
-        <Contact /> 
-        <Footer />
+    
+    <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
-   )
- }
- 
- export default App
+  );
+};
+
+export default App;
