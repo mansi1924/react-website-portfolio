01a8337d2fe28c10cdbba2e2af74ff169a9761e1
Mansi2419 <mg5111417@gmail.com>
2023-02-25 11:00:01 +0530

    25/02/23

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" style="margin:4rem;">
       <h5>Get To Know</h5>
       <h2>About US</h2>
 
